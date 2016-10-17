40984f30cd9f272759f4eb30c9a02ab84dbe4339
cuongdv <vietcuong2418@gmail.com>
2016-10-17 09:25:14 +0700

    cuong/change_store

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::pattern('id', '\d+');
 /**
  * Route Api
  */
@@ -23,6 +23,85 @@ Route::group(['prefix' => '/api/v1', 'namespace' => 'Api'], function() {
     /*
      * Route Users
      */
+    post('users', ['as' => 'api.users.post.index', 'uses' => 'UsersController@postCreate']);
     get('users', ['as' => 'api.users.get.index', 'uses' => 'UsersController@index']);
+
+    /*
+     * Route Events
+     */
+    get('/events', ['as' => 'api.events.get.index', 'uses' => 'EventsController@getEvents']);
+    get('/event/{id}', ['as' => 'api.event.get.show', 'uses' => 'EventsController@getEvent']);
+    post('/events', ['as' => 'api.events.post.create', 'uses' => 'EventsController@postCreate']);
+    post('/event/{id}', ['as' => 'api.events.put.update', 'uses' => 'EventsController@postUpdate']);
+    delete('/events/{id}', ['as' => 'api.events.get.delete', 'uses' => 'EventsController@delete']);
+    post('/events/{id}/join', ['as' => 'api.events.join.post.create', 'uses' => 'EventsController@postCreateJoinEvent']);
+    delete('/events/join/{id}', ['as' => 'api.events.delete.leave', 'uses' => 'EventsController@deleteLeaveEvent']);
+
+
+    /*
+     * Rout PrPoint
+     */
+    get('/prevents', ['as' => 'api.prevents.get.index', 'uses' => 'PrPointController@index']);
+    get('/prevents/{id}', ['as' => 'api.prevents.get.show', 'uses' => 'PrPointController@getShow']);
+    post('/prevents', ['as' => 'api.prevents.post.create', 'uses' => 'PrPointController@postCreate']);
+    post('/prevents/{id}', ['as' => 'api.prevents.post.create', 'uses' => 'PrPointController@postUpdate']);
+    delete('/prevents/{id}', ['as' => 'api.prevents.delete.create', 'uses' => 'PrPointController@delete']);
+
+    /*
+     * Route Ecategories
+     */
+    post('/ecategories', ['as' => 'api.ecategories.post.create', 'uses' => 'EventCategoriesController@postCreate']);
+    put('/ecategories/{id}', ['as' => 'api.ecategories.put.create', 'uses' => 'EventCategoriesController@putUpdate']); 
+    get('/ecategories', ['as' => 'api.ecategories.get.index', 'uses' => 'EventCategoriesController@getIndex']);
+    get('/ecategories/{id}', ['as' => 'api.ecategories.get.show', 'uses' => 'EventCategoriesController@getShow']);
+    delete('/ecategories/{id}', ['as' => 'api.ecategories.get.delete', 'uses' => 'EventCategoriesController@delete']);
+    /*
+    * Route Groups
+    */
+    get('/groups', ['as' => 'api.group.get.index', 'uses' => 'GroupsController@getIndex']);
+    post('/groups', ['as' => 'api.group.post.create', 'uses' => 'GroupsController@postCreate']);
+    post('/groups/{id}', ['as' => 'api.group.put.create', 'uses' => 'GroupsController@putUpdate']);
+    get('/groups/{id}', ['as' => 'api.group.get.show', 'uses' => 'GroupsController@getShow']);
+    delete('/groups/{id}', ['as' => 'api.group.get.delete', 'uses' => 'GroupsController@delete']);
+    post('/groups/{id}/upload', ['as' => 'api.group.upload.image', 'uses' => 'GroupsController@postUploadImage']);
+    post('/groups/{id}/join', ['as' => 'api.group.join.post.create', 'uses' => 'GroupsController@postCreateJoinGroup']);
+    delete('/groups/join/{id}', ['as' => 'api.group.get.leave', 'uses' => 'GroupsController@deleteLeaveGroup']);
+
+    /**
+     *Rout MyPage
+     */
+     get('/mypage', ['as' => 'api.mypage.get.index', 'uses' => 'MyPageController@getIndex']);
+     get('/mypage/event', ['as' => 'api.mypage.get.index', 'uses' => 'MyPageController@getAllEvent']);
+     get('/mypage/event/{id}', ['as' => 'api.mypage.get.index', 'uses' => 'MyPageController@getDetailEvent']);
+
+    /*
+    * Search Event
+    */
+    get('search-events', ['as' => 'api.search-events.get', 'uses' => 'SearchController@searchEvents']);
+    get('search-events-group', ['as' => 'api.search-events-group.get', 'uses' => 'SearchController@searchEventsGroup']);
+    get('event-detail', ['as' => 'api.event.get', 'uses' => 'SearchController@getEvent']);
+    /*
+    * Related Event
+    */
+    get('related-event/{id}', ['as' => 'api.related-event.get', 'uses' => 'EventsController@getRelatedEvent']);
+    /*
+    * update User Profile
+    */
+    post('user-profile/{id}', ['as' => 'api.user-profile.update', 'uses' => 'UserProfileController@postUpadte']);
+    put('user-profile-notifi/{id}', ['as' => 'api.user-profile-notifi.update', 'uses' => 'UserProfileController@NotificationUpdate']);
+    /*
+    * Buy Event
+    */
+    get('event-packges', ['as' => 'api.event-packges.get', 'uses' => 'UserPlanController@getList']);
+    post('event/{id}/buy', ['as' => 'api.buy-event.post', 'uses' => 'UserPlanController@postBuyEvent']);
+    /*
+    * Took Place Event
+    */
+    get('took-place-events/{group_id}', ['as' => 'api.took-place-events.get', 'uses' => 'EventsController@getTookPlaceEvents']);
+    /*
+    * Events Plan
+    */
+    get('events-plan/{group_id}', ['as' => 'api.events-plan.get', 'uses' => 'EventsController@getEventsPlan']);
+
 
 });
---
+++ app/Repositories/SearchEventsRepository.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Repositories;
+use App\Models\Event;
+use App\Models\Group;
+use App\Models\EventCategory;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+
+class SearchEventsRepository extends AbstractRepository {
+
+    protected $models;
+    protected $groups;
+
+    public function __construct(Event $events, Group $event_groups) {
+        $this->models = $events;
+        $this->groups = $event_groups;
+    }
+
+    public function searchEvents($params,$page = 0, $attributes = ['*']) {
+        $builder = $this->models;
+        if (isset($params['name'])) {
+            $builder = $builder->where('name','LIKE', '%'.$params['name'].'%');
+        }
+        if (isset($params['cat_id'])) {
+            $builder = $builder->where('cat_id', $params['cat_id']);
+        }
+        if (isset($params['user_max'])) {
+            $builder = $builder->where('user_max', $params['user_max']);
+        }
+        
+        if (isset($params['address'])) {
+            $builder = $builder->where('address','LIKE', '%'.$params['address'].'%');
+        }
+
+
+       
+        
+        if (!empty(($params['lat']) && ($params['long']) &&($params['radius'])) ) {
+           $builder = DB::select("
+            SELECT
+              *, (
+                3959 * acos (
+                  cos ( radians(?) )
+                  * cos( radians( `lat` ) )
+                  * cos( radians( `long` ) - radians(?) )
+                  + sin ( radians(?) )
+                  * sin( radians( `lat` ) )
+                )
+              ) AS distance
+            FROM line_events
+            HAVING distance <= ?
+            ORDER BY distance
+            ", array($params['lat'], $params['long'], $params['lat'], $params['radius']));
+        }
+        
+            return $builder;
+            // $result = $builder->paginate($attributes);
+            // return $result->toArray();
+        
+    }
+
+
+     public function searchEventGroups($params) {
+        $event_groups = $this->groups;
+        if (isset($params['name'])) {
+            $event_groups = $event_groups->where('name',$params['name'])->with('event');
+        }
+        if (!empty(($params['lat']) && ($params['long']) &&($params['radius'])) ) {
+           $event_groups = DB::select("
+            SELECT
+              *, (
+                3959 * acos (
+                  cos ( radians(?) )
+                  * cos( radians( `lat` ) )
+                  * cos( radians( `long` ) - radians(?) )
+                  + sin ( radians(?) )
+                  * sin( radians( `lat` ) )
+                )
+              ) AS distance
+            FROM line_groups
+            HAVING distance <= ?
+            ORDER BY distance
+            ", array($params['lat'], $params['long'], $params['lat'], $params['radius']));
+        }
+        return $result = [
+            'event_groups' => $event_groups
+        ];
+    }
+
+
+    public function getEvent($params)
+    {
+        $event = $this->models->Where('name',$params['name'])->with(['event_group', 'event_category'])->get();
+        return $event;
+    }
+
+}
