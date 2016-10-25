88ac95111b9fe4a7e281d10b7e96875d4b1ca2d7
phamduy <phamdinhduy2808@gmail.com>
2016-10-25 10:50:38 +0700

    update and fix

--- app/Http/Requests/CreateEventRequest.php
+++ app/Http/Requests/CreateEventRequest.php
@@ -37,7 +37,10 @@ class CreateEventRequest extends Request
             'address'           => 'required',
             'user_id'           => 'required',
             'group_id'          => 'required'
-            /*required for image max:50 'photo' => 'mimes:jpeg,bmp,png' 'avatar' => 'dimensions:min_width=100,min_height=200'*/
+            /*required for image
+             'avatar' => 'dimensions:min_width=100,min_height=200'
+             'picture' => 'required | mimes:jpeg,jpg,png,bmp | max:1000',
+             */
 
         ];
     }
